56770532a3cc92f19a8c2d614a2ce548d62b25ab
Evonic4 <gevonic@gmail.com>
2024-08-19 09:16:35 +0300

    0.2

--- starter.sh
+++ starter.sh
@@ -4,11 +4,13 @@ ver="v0.2"
 fhome=/usr/share/norit/
 
 
-function Init2() 
+function init() 
 {
 logger "Init"
 pushg_start=$(sed -n 1"p" $fhome"sett.conf" | tr -d '\r')
 pushg_port=$(sed -n 3"p" $fhome"sett.conf" | tr -d '\r')
+logger "Init pushg_start="$pushg_start
+logger "Init pushg_port="$pushg_port
 }
 
 function logger()
@@ -21,8 +23,11 @@ echo $date1" norit starter: "$1
 #START
 logger " "
 logger "start, ver "$ver
+init;
 
 cd $fhome"sc/"
+logger "setup scripts"
+pwd
 perl -pi -e "s/\r\n/\n/" ./setup.sh
 chmod +rx ./setup.sh
 ./setup.sh
@@ -38,6 +43,7 @@ if [ "$pushg_start" == "1" ]; then
 fi
 sleep 1
 
+logger "find confs"
 find $fhome"conf/" -maxdepth 1 -type f -name '*.config' > $fhome"confs1.txt"
 str_col1=$(grep -c '' $fhome"confs1.txt")
 logger "str_col1="$str_col1
@@ -48,6 +54,7 @@ for (( i1=1;i1<=$str_col1;i1++)); do
 	$fhome"norit.sh" $i1 &
 done
 
+logger "confs2.txt list"
 cat $fhome"confs2.txt"
 
 while true
